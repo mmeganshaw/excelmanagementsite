6c603f076c9c34d0731f5abd4be76d02a33bf44b
Megan Shaw <mmeganshaw@gmail.com>
2020-12-27 19:25:58 -0500

    Adding components for home page.

---
+++ public/js/index.js
@@ -0,0 +1,12 @@
+(function($){
+    $(function(){
+
+      $('.sidenav').sidenav();
+      $('.carousel').carousel();
+      $('.collapsible').collapsible();
+      $('select').formSelect();
+      $('.modal').modal();
+      $(".dropdown-trigger").dropdown();
+        
+    }); // end of document ready
+  })(jQuery); // end of jQuery name space
