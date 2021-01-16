049a30359651560579f026f7d183f9dd512b2506
Megan Shaw <mmeganshaw@gmail.com>
2021-01-16 13:29:40 -0500

    Adding Contact Page.

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,10 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname + "/../public/about-us.html"))
     })
 
+    app.get("/contact", function(req,res) {
+        res.sendFile(path.join(__dirname + "/../public/contact.html"))
+    })
+
     app.get("*", function(req,res) {
         res.sendFile(path.join(__dirname + "/../public/index.html"))
     });
