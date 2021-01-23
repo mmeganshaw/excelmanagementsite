ef290270f619e1d9958a1737f9fd99d4ebaa1169
Megan Shaw <mmeganshaw@gmail.com>
2021-01-23 15:08:32 -0500

    Adding Snow Services page.

--- routes/index.js
+++ routes/index.js
@@ -9,11 +9,11 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname + "/../public/index.html"))
     });
 
-    app.get("/about", function(req,res) {
+    app.get("/about", function(req, res) {
         res.sendFile(path.join(__dirname + "/../public/about-us.html"))
     })
 
-    app.get("/contact", function(req,res) {
+    app.get("/contact", function(req, res) {
         res.sendFile(path.join(__dirname + "/../public/contact.html"))
     })
 
@@ -25,7 +25,23 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname + "/../public/customer-survey.html"))
     })
 
-    app.get("*", function(req,res) {
+    app.get("/snow-ice-management", function(req, res) {
+        res.sendFile(path.join(__dirname + "/../public/snow.html"))
+    })
+
+    app.get("/irrigation", function(req, res) {
+        res.sendFile(path.join(__dirname + "/../public/irrigation.html"))
+    })
+
+    app.get("/landscaping", function(req, res) {
+        res.sendFile(path.join(__dirname + "/../public/landscaping.html"))
+    })
+
+    app.get("/construction", function(req, res) {
+        res.sendFile(path.join(__dirname + "/../public/construction.html"))
+    })
+
+    app.get("*", function(req, res) {
         res.sendFile(path.join(__dirname + "/../public/index.html"))
     });
 }
